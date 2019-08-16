b88c7deb49bbd220f515e51db9eda41e7d9a8995
Stefen Wakefield <xstefen@gmail.com>
2019-08-15 20:25:29 -0500

    sepolicy: fod
    Signed-off-by: Stefen Wakefield <xstefen@gmail.com>
    Change-Id: I12b01418ece248bc724714a313969ea9788cb74e

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,3 +1,8 @@
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/statix/sepolicy/common/public
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/statix/sepolicy/common/private
 
+BOARD_SEPOLICY_DIRS += \
+    device/statix/sepolicy/common/vendor
