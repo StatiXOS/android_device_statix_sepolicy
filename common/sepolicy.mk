0951ca4326209ed2908d84f0b806ba2b1350dc0d
ralph950412 <ralph950412@gmail.com>
2025-02-12 10:10:41 +0100

    fixup! sepolicy: include flipendo/turbo_adapter policy
    Change-Id: I96cbc99a0fb563aada33aa41f9804853f9095816

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,20 +10,19 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/statix/sepolicy/common/public
+    device/statix/sepolicy/common/public \
+    hardware/google/pixel-sepolicy/turbo_adapter/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/statix/sepolicy/common/private
+    device/statix/sepolicy/common/private \
+    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/turbo_adapter/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/statix/sepolicy/common/dynamic \
-    hardware/google/pixel-sepolicy/flipendo \
-    hardware/google/pixel-sepolicy/turbo_adapter
+    device/statix/sepolicy/common/dynamic
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/statix/sepolicy/common/dynamic \
-    device/statix/sepolicy/common/vendor \
-    hardware/google/pixel-sepolicy/flipendo \
-    hardware/google/pixel-sepolicy/turbo_adapter
+    device/statix/sepolicy/common/vendor
 endif
